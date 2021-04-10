9ca96317adc1c7aba7c9a9c4a1d614a4f040335c
backflip270bb <backflip270bb@gmail.com>
2021-04-10 17:18:12 +0900

    Store values in map

--- background.js
+++ background.js
@@ -1,86 +1,145 @@
 function getData() {
+  var valueMap = new Map();
+
+  var parseTableRow = function(trElem) {
+    var header = trElem.querySelector("th").textContent
+    var value = trElem.querySelector("td").textContent
+    //console.log(header + ":" + value)
+    valueMap.set(header, value)
+  };
+
+  var parseTableRowAsYen = function(trElem) {
+    var header = trElem.querySelector("th").textContent
+    var value = trElem.querySelector("td").textContent // 1,230円00銭
+    var vars = value.match(/([+-\d,]*)円(\d+)銭/);
+    var fs = vars[1] + "." + vars[2]; 
+    //console.log(header + ":" + fs)
+    valueMap.set(header, fs)
+  };
+
+  var output = function() {
+      valueMap.forEach(function(value, key) {
+        console.log(key + ":" + value)
+      })
+  }
+
   // 対象年月
-  var target = document.querySelector("#tgtYmDisp option[selected='selected']").text // 2021年03月
-  console.log(target)
+  var str = document.querySelector("#tgtYmDisp option[selected='selected']").text // 2021年03月
+  vars = str.match(/(\d+)年(\d+)月/)
+  var year = vars[1]
+  var month = vars[2]
+  valueMap.set("対象年月", year + "-" + month)
 
   // 請求金額
-  var monthlyCharge = document.querySelectorAll(".info-content-main .info-price")[0].textContent // "12,345円"
-  var taxIncluded = document.querySelectorAll(".info-content-main p")[1].textContent // "（ うち消費税等相当額 1,423円 ）"
-  
-  // 使用量
-  var usedEnergyMain = document.querySelectorAll(".info-content-main .info-price")[1].textContent // "567kWh"
+  var str = document.querySelectorAll(".info-content-main .info-price")[0].textContent // "12,345円"
+  var vars = str.match(/([\d,]+)円/)
+  monthlyCharge = vars[1]
+  valueMap.set("請求金額", monthlyCharge)
 
-  console.log(monthlyCharge)
-  console.log(taxIncluded)
-  console.log(usedEnergyMain)
+  str = document.querySelectorAll(".info-content-main p")[1].textContent // "（ うち消費税等相当額 1,423円 ）"
+  vars = str.match(/.* ([\d,]+)円/)
+  taxIncluded = vars[1]
+  valueMap.set("消費税相当額", taxIncluded)
+
+  // 使用量
+  str = document.querySelectorAll(".info-content-main .info-price")[1].textContent // "567kWh"
+  vars = str.match(/([\d,]+)kWh/)
+  usedEnergyMain = vars[1]
+  valueMap.set("使用量[kWh]", usedEnergyMain)
 
   // 料金テーブル
   var costTableRows = document.querySelectorAll(".info-tbl .info-toggle-body tr")
   // 基本料金
-  var basicCharge = costTableRows[0].querySelector("td").textContent // 1,230円00銭
+  var basicCharge = costTableRows[0] // 1,230円00銭
+  parseTableRowAsYen(basicCharge);
   // １段料金
-  var charge1 = costTableRows[2].querySelector("td").textContent // "1,234円56銭"
+  var charge1 = costTableRows[2] // "1,234円56銭"
+  parseTableRowAsYen(charge1);
+
   // ２段料金
-  var charge2 = costTableRows[3].querySelector("td").textContent // "1,234円56銭"
+  var charge2 = costTableRows[3] // "1,234円56銭"
+  parseTableRowAsYen(charge2);
+
   // ３段料金
-  var charge3 = costTableRows[4].querySelector("td").textContent // "1,234円56銭"
+  var charge3 = costTableRows[4] // "1,234円56銭"
+  parseTableRowAsYen(charge3);
+
   // 燃料費調整額
-  var adjcharge = costTableRows[5].querySelector("td").textContent // "-1,234円00銭"
+  var adjcharge = costTableRows[5] // "-1,234円00銭"
+  parseTableRowAsYen(adjcharge);
+
   // 再エネ発電賦課金
-  var surcharge = costTableRows[6].querySelector("td").textContent // "1,234円00銭"
+  var surcharge = costTableRows[6] // "1,234円00銭"
+  parseTableRowAsYen(surcharge);
+
   // 口座振替割引
-  var discount = costTableRows[7].querySelector("td").textContent // "-55円00銭"
+  var discount = costTableRows[7] // "-55円00銭"
+  parseTableRowAsYen(discount);
 
  
   var tables = document.querySelectorAll(".info-tbl")
 
   // 使用量テーブル
   var usedRows = tables[3].querySelectorAll("tr")
-  var duration = usedRows[0].querySelector("td").textContent // "2月10日～ 3月11日"
-  var inspectionDate = usedRows[1].querySelector("td").textContent //"3月12日 （30日間）"
+  // 使用期間
+  var trElem = usedRows[0] // "2月10日～ 3月11日"
+  var header = trElem.querySelector("th").textContent
+  var value = trElem.querySelector("td").textContent
+  vars = value.match(/(\d+)月(\d+)日～ (\d+)月(\d+)日/)
+  var duration = vars[1] + "/" + vars[2] + "-" + vars[3] + "/" + vars[4]
+  valueMap.set(header, duration)
 
-  console.log(duration)
-  console.log(inspectionDate)
+  // 検針月日
+  var trElem = usedRows[1] //"3月12日 （30日間）"
+  var header = trElem.querySelector("th").textContent
+  var value = trElem.querySelector("td").textContent
+  vars = value.match(/(\d+)月(\d+)日 （(\d+)日間）/)
+  var inspectionDate = year + "/" + vars[1] + "/" + vars[2]
+  var durationDays = vars[3]
+  valueMap.set(header, inspectionDate)
+  valueMap.set("対象日数", durationDays)
 
   // 燃料費調整額テーブル
   var adjRows = tables[4].querySelectorAll("tr")
   // 当月分
-  var adjustRate = adjRows[0].querySelector("td").textContent // "-1円23銭"
-  // 翌月分
-  var nextAdjustRate = adjRows[1].querySelector("td").textContent // "-1円23銭"
-  // 翌月は当月に比べ
-  var adjustRateDiff = adjRows[2].querySelector("td").textContent // "+0円12銭"
+  var adjustRate = adjRows[0] // "-1円23銭"
+  parseTableRowAsYen(adjustRate);
 
-  console.log(adjustRate)
-  console.log(nextAdjustRate)
-  console.log(adjustRateDiff)
+  // 翌月分
+  var nextAdjustRate = adjRows[1] // "-1円23銭"
+  parseTableRowAsYen(nextAdjustRate);
+
+  // 翌月は当月に比べ
+  var adjustRateDiff = adjRows[2] // "+0円12銭"
+  parseTableRowAsYen(adjustRateDiff);
+
 
   // その他テーブル
   var otherRows = tables[5].querySelectorAll("tr")
   // 再エネ発電賦課金単価　（1kWhあたり）
-  var surchargeRate = otherRows[0].querySelector("td").textContent // "1円23銭"
-  // 託送料金平均単価（1kWhあたり）
-  var consignmentRate = otherRows[1].querySelector("td").textContent // "1円23銭"
+  var surchargeRate = otherRows[0] // "1円23銭"
+  parseTableRowAsYen(surchargeRate);
 
-  console.log(surchargeRate)
-  console.log(consignmentRate)
+  // 託送料金平均単価（1kWhあたり）
+  var consignmentRate = otherRows[1] // "1円23銭"
+  parseTableRowAsYen(consignmentRate);
 
   // 計器情報テーブル
   var meterRows = tables[6].querySelectorAll("tr")
   // 当月指示数
-  var meterCount = meterRows[0].querySelector("td").textContent // "12345.6"
+  var meterCount = meterRows[0] // "12345.6"
+  parseTableRow(meterCount)
   // 前月指示数
-  var lastMeterCount = meterRows[1].querySelector("td").textContent // "12345.6"
+  var lastMeterCount = meterRows[1] // "12345.6"
+  parseTableRow(lastMeterCount)
   // 差引
-  var diff = meterRows[2].querySelector("td").textContent // "123.4"
+  var diff = meterRows[2] // "123.4"
+  parseTableRow(diff)
   // 使用電力量
-  var usedEnergy = meterRows[3].querySelector("td").textContent // "123"
+  var usedEnergy = meterRows[3] // "123"
+  parseTableRow(usedEnergy)
 
-  console.log(meterCount)
-  console.log(lastMeterCount)
-  console.log(diff)
-  console.log(usedEnergy)
-
+  output()
 }
 
 chrome.action.onClicked.addListener((tab) => {
